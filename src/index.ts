946be33620d377bd87db40ab7d1851fc33d278dd
lycaon <oka.lka@gmail.com>
2020-06-18 20:40:15 +0900

    feat: add currentBranch pre-defined variable.

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,8 @@ function isServerlessIdobataWebhookCustom(
 }
 
 class ServerlessIdobataWebhook {
+  private static logPrefix = '[idobata-webhook] ';
+
   public hooks = {
     'before:deploy:deploy': this.beforeDeploy.bind(this),
     'after:deploy:deploy': this.afterDeploy.bind(this),
@@ -48,35 +50,41 @@ class ServerlessIdobataWebhook {
     this.customData = this.extractCustom();
   }
 
-  beforeDeploy(): void {
+  async beforeDeploy(): Promise<void> {
     this.notifyIdobata(
-      formatMessage(
+      await formatMessage(
         this.customData?.beforeDeployMessage ??
-          '{currentTime} [{username}@{hostname}] deploy started!'
+          '{currentTime} [{username}@{hostname}] ({currentBranch}) deploy started!'
       )
     );
   }
 
-  afterDeploy(): void {
+  async afterDeploy(): Promise<void> {
     this.notifyIdobata(
-      formatMessage(
+      await formatMessage(
         this.customData?.afterDeployMessage ??
-          '{currentTime} [{username}@{hostname}] deploy finished!'
+          '{currentTime} [{username}@{hostname}] ({currentBranch}) deploy finished!'
       )
     );
   }
 
   async notifyIdobata(message: string): Promise<void> {
     if (!this.customData?.webhookUrl) {
-      this.serverless.cli.log(chalk.red('***** no webhook url in custom.'));
+      this.errorLog('***** no webhook url in custom.');
       return;
     }
 
-    const body = new FormData();
-    body.append('source', message);
-    await got.post(this.customData.webhookUrl, {
-      body,
-    });
+    try {
+      const body = new FormData();
+      body.append('source', message);
+      await got.post(this.customData.webhookUrl, {
+        body,
+      });
+    } catch (e) {
+      this.errorLog(
+        `***** error occurred at send notification to idobata: ${e}`
+      );
+    }
   }
 
   extractCustom(): ServerlessIdobataWebhookCustom | undefined {
@@ -88,6 +96,16 @@ class ServerlessIdobataWebhook {
     } else {
       return;
     }
+  }
+
+  errorLog(message: string): void {
+    this.serverless.cli.log(
+      chalk.red(`${ServerlessIdobataWebhook.logPrefix}${message}`)
+    );
+  }
+
+  infoLog(message: string): void {
+    this.serverless.cli.log(`${ServerlessIdobataWebhook.logPrefix}${message}`);
   }
 }
 
--- src/message.ts
+++ src/message.ts
@@ -1,18 +1,42 @@
 import * as DateFns from 'date-fns';
+import fs from 'fs';
 import ip from 'ip';
+import git from 'isomorphic-git';
 import os from 'os';
+import path from 'path';
 import pupa from 'pupa';
 
-export function formatMessage(templateText: string): string {
+export async function formatMessage(templateText: string): Promise<string> {
   const hostname = os.hostname();
   const currentTime = DateFns.format(new Date(), 'yyyy-MM-dd HH:mm:ss');
   const username = os.userInfo().username;
   const ipAddress = ip.address();
+  const currentBranch = await git
+    .currentBranch({
+      fs,
+      dir: searchGitDir(process.cwd()),
+      fullname: false,
+    })
+    .catch(() => '[NO CURRENT BRANCH]');
 
   return pupa(templateText, {
     hostname,
     currentTime,
     username,
     ipAddress,
+    currentBranch,
   });
+}
+
+function searchGitDir(gitDir: string, maxSearch: number = 0): string {
+  if (maxSearch > 2) {
+    return '[NO CURRENT BRANCH]';
+  }
+
+  try {
+    fs.accessSync(path.resolve(gitDir, '.git'), fs.constants.R_OK);
+    return gitDir;
+  } catch (e) {
+    return searchGitDir(path.resolve(gitDir, '..'), ++maxSearch);
+  }
 }
